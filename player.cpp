9af279549a4f0f60faf4033e0c3b1b9a06d050fe
deshkvla <deshkvla@fel.cvut.cz>
2024-05-19 18:20:01 +0200

    upd player

--- player.cpp
+++ player.cpp
@@ -1,15 +1,20 @@
 #include "player.h"
 
+Player::Player(int x, int y, int width, int height, int speed):
+    Player::Player(x, y, width, height) {
+    this->speed = speed;
+}
+
 void Player::draw() {
   knobs_controller->update();
   if (knobs_controller->is_red_pressed) {
     if (x < 450) {
-      x += 3;
+      x += speed;
     }
   }
   if (knobs_controller->is_blue_pressed) {
     if (x > 0) {
-      x -= 3;
+      x -= speed;
     }
   }
   render_controller->drawShip(x, width);
--- player.h
+++ player.h
@@ -8,6 +8,7 @@ private:
     KnobsController *knobs_controller;
 public:
     using Entity::Entity;
+    Player(int x, int y, int width, int height, int speed);
     void draw();
     void setKnobsController(KnobsController *knobs_controller);
 };
