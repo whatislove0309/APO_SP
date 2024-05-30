c8b06170377022ab8f7925dd6e12f2b857c62de0
deshkvla <deshkvla@fel.cvut.cz>
2024-05-30 13:36:13 +0200

    upd score

--- collision_controller.h
+++ collision_controller.h
@@ -1,12 +1,18 @@
 #pragma once
+#include <functional>
+#include <vector>
+
 #include "asteroid.h"
 #include "bullet.h"
 #include "entity.h"
 #include "player.h"
-#include <vector>
 
 class CollisionController {
+private:
+  std::function<void()> updateScoreCallback;
+
 public:
+  CollisionController(std::function<void()> callback);
   bool checkCollision(Entity *entity_1, Entity *entity_2);
   void checkBulletCollision(std::vector<Bullet *> *bullets,
                             std::vector<Asteroid *> *asteroids);
