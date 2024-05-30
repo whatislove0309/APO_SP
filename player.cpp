0f6057da015d6ce5dfa45ace99242d4fe3a980e3
deshkvla <deshkvla@fel.cvut.cz>
2024-05-30 12:50:09 +0200

    add recovery

--- player.cpp
+++ player.cpp
@@ -1,17 +1,20 @@
 #include "player.h"
 
-Player::Player(RenderController *render_controller, 
-  KnobsController *knobs_controller, DiodController *diod_controller)
+Player::Player(RenderController *render_controller,
+               KnobsController *knobs_controller,
+               DiodController *diod_controller)
     : Player::Player(render_controller, 213, 238, 54, 62) {
   this->knobs_controller = knobs_controller;
   this->diod_controller = diod_controller;
   this->speed = 10;
   this->health = maxHealth;
-  printf("player diod controller: %p\n", diod_controller);
+  this->lastRecoveryTime = std::chrono::steady_clock::now();
 }
 
 void Player::draw() {
+  using namespace std::chrono;
   diod_controller->diod(health);
+
   if (knobs_controller->is_blue_pressed) {
     if (x < 480 - width) {
       x += speed;
@@ -22,13 +25,26 @@ void Player::draw() {
       x -= speed;
     }
   }
+
+  auto now = steady_clock::now();
+  auto sinceLast = duration_cast<seconds>(now - lastRecoveryTime).count();
+  if (sinceLast >= recoveryInterval) {
+    recover();
+    lastRecoveryTime = now;
+  }
+
   render_controller->drawShip(x);
 }
 
-int Player::getHealth() {
-  return health;
-}
+int Player::getHealth() { return health; }
 
 void Player::damage() {
-  if (health > 0) health--;
-} 
+  if (health > 0)
+    health--;
+}
+
+void Player::recover() {
+  if (health < maxHealth) {
+    health++;
+  }
+}
--- player.h
+++ player.h
@@ -1,8 +1,10 @@
 #pragma once
 #include <vector>
-#include "entity.h"
+#include <chrono>
+
 #include "bullet.h"
 #include "diod_controller.h"
+#include "entity.h"
 
 class Player : public Entity {
 private:
@@ -10,12 +12,15 @@ private:
   DiodController *diod_controller;
   int health;
   int maxHealth = 4;
+  std::chrono::time_point<std::chrono::steady_clock> lastRecoveryTime;
+  const int recoveryInterval = 15;
 
 public:
   using Entity::Entity;
-  Player(RenderController *render_controller, KnobsController *knobs_controller, 
-    DiodController *diod_controller);
+  Player(RenderController *render_controller, KnobsController *knobs_controller,
+         DiodController *diod_controller);
   void draw();
   int getHealth();
   void damage();
+  void recover();
 };
